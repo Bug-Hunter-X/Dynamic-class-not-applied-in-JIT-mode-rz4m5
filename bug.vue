e8f789ab484cb5bf17e6b54894f12ab70f7df55d
BugHunter-X <askzenai@gmail.com>
2025-01-05 05:04:07 +0100

    Add bug.vue file

---
+++ bug.vue
@@ -0,0 +1,18 @@
+This bug occurs when using Tailwind CSS with dynamically generated content.  The issue is that Tailwind's JIT (Just-In-Time) mode might not pick up classes that are added after the initial page load. This usually happens when you add or remove elements using JavaScript, Vue, React, or similar frameworks.  The dynamic classes are not recognized, resulting in no styling being applied.
+
+Example (Vue):
+```vue
+<template>
+  <div :class="['bg-blue-500', dynamicClass]" >Dynamic Content</div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+      dynamicClass: 'text-white'
+    };
+  },
+};
+</script>
+```
